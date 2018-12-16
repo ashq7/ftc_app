f3f854943e75f78d4cb035658114bb61ccba543a
Aaron Cook <cookx770@umn.edu>
2018-12-15 22:38:00 -0600

    Adding Bridget's test program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_programs/MoveRight.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.test_programs;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive_trains.HolonomicDrive;
+
+@Autonomous(name = "Moveright",group ="Test")
+public class MoveRight extends LinearOpMode {
+    @Override
+    public void runOpMode () {
+        HolonomicDrive Drivetrain;
+        Drivetrain = new HolonomicDrive(hardwareMap);
+        Drivetrain.init();
+
+        waitForStart();
+        Drivetrain.pan(Math.PI/2.0, 0.5);
+        sleep(5000);
+        Drivetrain.stop();
+    }
+}
